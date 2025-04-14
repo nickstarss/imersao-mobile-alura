9ec8657183bee2768498cb1cb36b2493edacba82
giuliana.pinton <giuliana.pinton@gmail.com>
2025-04-14 19:11:56 +0000

    first commit: commiting the learning of the first lesson of the course, creating the splash page and stting the enviroment

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/ui/_core/app_theme.dart';
+import 'package:myapp/ui/splash/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: AppTheme.appTheme,
+      home: SplashScreen(),
+    );
+  }
+}
