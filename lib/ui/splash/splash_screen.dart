f4f72033d3e2134b0b2ad43368fbce67752818f0
giuliana.pinton <giuliana.pinton@gmail.com>
2025-04-16 12:50:30 +0000

    att: commit of the second class of the course of alura, crating a logic for the home page

--- lib/ui/splash/splash_screen.dart
+++ lib/ui/splash/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/ui/_core/app_colors.dart';
+import 'package:myapp/ui/home/home_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
@@ -23,25 +24,34 @@ class SplashScreen extends StatelessWidget {
                     children: [
                       Text(
                         "Um parceiro inovador para sua",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 22,
-                        )
+                        style: TextStyle(color: Colors.white, fontSize: 22),
                       ),
                       Text(
                         "melhor experência culinária!",
                         style: TextStyle(
                           color: AppColors.mainColor,
                           fontSize: 22,
-                          fontWeight: FontWeight.bold
-                        )
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ],
                   ),
                   SizedBox(
                     width: double.infinity,
-                    child: ElevatedButton(onPressed: (){}, child: Text("Bora!"))
-                  )
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return HomeScreen();
+                            },
+                          ),
+                        );
+                      },
+                      child: Text("Bora!"),
+                    ),
+                  ),
                 ],
               ),
             ),
