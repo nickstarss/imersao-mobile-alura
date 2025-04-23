e331f5d0e1ce84c5776c28f8e4e25982890bcd39
giuliana.pinton <giuliana.pinton@gmail.com>
2025-04-23 18:05:35 +0000

    last commit: last commit with the full course content about Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:myapp/data/restaurant_data.dart';
 import 'package:myapp/ui/_core/app_theme.dart';
+import 'package:myapp/ui/_core/bag_provider.dart';
 import 'package:myapp/ui/splash/splash_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -16,6 +17,7 @@ void main() async {
             return restaurantData;
           },
         ),
+        ChangeNotifierProvider(create: (context)=> BagProvider())
       ],
       child: MyApp(),
     ),
---
+++ lib/ui/restaurant/restaurant_screen.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/model/dish.dart';
+import 'package:myapp/model/restaurant.dart';
+import 'package:myapp/ui/_core/bag_provider.dart';
+import 'package:myapp/ui/_core/widgets/appBar.dart';
+import 'package:provider/provider.dart';
+
+class RestaurantScreen extends StatelessWidget {
+  final Restaurant restaurant;
+  const RestaurantScreen({super.key, required this.restaurant});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: getAppBar(context: context, title: restaurant.name),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 26.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          spacing: 12,
+          children: [
+            Center(
+              child: Image.asset("assets/${restaurant.imagePath}", width: 128),
+            ),
+            Text("Mais pedidos", style: TextStyle(fontWeight: FontWeight.bold)),
+            Column(
+              children: List.generate(restaurant.dishes.length, (index) {
+                Dish dish = restaurant.dishes[index];
+                return ListTile(
+                  leading: Image.asset(
+                    'assets/dishes/default.png',
+                    width: 48,
+                    height: 48,
+                  ),
+                  title: Text(dish.name),
+                  subtitle: Text("R\$${dish.price.toStringAsFixed(2)}"),
+                  trailing: IconButton(
+                    onPressed: () {
+                      context.read<BagProvider>().addAllDishes([dish]);
+                    },
+                    icon: Icon(Icons.add),
+                  ),
+                );
+              }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
